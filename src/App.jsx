0777324104aae83336dc53e41a7ca4129e6d30c6
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-02 23:19:32 +0530

    setup basic with vite + react, tailwind, and react router

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <h1 class="text-3xl font-bold underline">Hello world!</h1>
+            <Routes>
+                <Route />
+            </Routes>
+        </>
+    );
+}
+
+export default App;
