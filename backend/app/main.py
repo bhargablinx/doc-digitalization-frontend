61336e457a3c51d74b00768b68634a1f6db179c9
Nilotpal04 <nilotpalb2004@gmail.com>
2026-06-04 11:41:48 +0000

    separate data processing logic from main.py to dedicated module

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,12 @@
 from fastapi import FastAPI, UploadFile
-import shutil
-
-from app.services.pdf_extractor import extract_pdf_text
-from app.services.ocr_extractor import extract_image_text
-
-from app.parsers.pdf_parser import parse_pdf_text
-from app.parsers.ocr_parser import parse_ocr_text
-
 from fastapi.middleware.cors import CORSMiddleware
 
+import shutil
+
+from app.services.document_service import process_document
+
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -18,22 +15,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.post("/upload")
 async def upload_file(file: UploadFile):
 
     try:
+
         file_path = f"app/uploads/{file.filename}"
 
         with open(file_path, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
-        if file.filename.endswith(".pdf"):
-            text = extract_pdf_text(file_path)
-            data = parse_pdf_text(text)
-
-        else:
-            text = extract_image_text(file_path)
-            data = parse_ocr_text(text)
+        data = process_document(
+            file_path=file_path,
+            filename=file.filename
+        )
 
         return {
             "success": True,
@@ -42,10 +38,8 @@ async def upload_file(file: UploadFile):
         }
 
     except Exception as e:
+
         return {
             "success": False,
             "error": str(e)
         }
-    
-    
-    
